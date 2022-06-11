f5ef7bb9f5eeb4eedeb2143ddb94ef959e9d21d1
ManuelP2 <manuelperezp22@hotmail.com>
2022-06-11 12:10:18 -0400

    expiration logout logic completa

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { AppBar, Avatar, Button, Toolbar, Typography } from "@material-ui/core";
 import memories from '../../images/memories.png';
+import decode from 'jwt-decode'
 import { Link } from "react-router-dom";
 import { useDispatch } from 'react-redux';
 import { useNavigate, useLocation } from 'react-router-dom';
@@ -28,6 +29,12 @@ const Navbar = () => {
   //Muestra el usuario logeado sin necesidad de refrescar
   useEffect(() => {
     const token = user?.token;
+
+    if(token) {
+      const decodedToken = decode(token);
+
+      if(decodedToken.exp * 1000 < new Date().getTime()) logout();
+    }
 
     setUser(JSON.parse(localStorage.getItem('profile')));
   }, [location]);
