002bf063c90ebbe002fe346ddc51cb01c2f5f1df
ManuelP2 <manuelperezp22@hotmail.com>
2022-06-13 12:51:08 -0400

    logica de pagination lista

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -2,9 +2,11 @@ import { FETCH_ALL, FETCH_BY_SEARCH, CREATE, UPDATE, DELETE, LIKE } from '../con
 
 import * as api from '../api/index.js';
 
-export const getPosts = () => async (dispatch) => {
+export const getPosts = (page) => async (dispatch) => {
   try {
-    const { data } = await api.fetchPosts();
+    const { data } = await api.fetchPosts(page);
+
+    console.log(data)
 
     dispatch({ type: FETCH_ALL, payload: data });
   } catch (error) {
--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -28,9 +28,6 @@ const Home = () => {
     const [search, setSearch] = useState('');
     const [tags, setTags] = useState([]);
   
-    useEffect(() => {
-      dispatch(getPosts());
-    }, [currentId, dispatch]);
 
     const searchPost = () => {
       if(search.trim() || tags) {
@@ -66,7 +63,7 @@ const Home = () => {
               </AppBar>
               <Form currentId={currentId} setCurrentId={setCurrentId} />
               <Paper elevation={6}>
-                <Pagination />
+                <Pagination page={page} />
               </Paper>
             </Grid>
           </Grid>
